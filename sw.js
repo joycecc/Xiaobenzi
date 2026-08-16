633d9a6dcb78e397419059015424d5ee9fdd0d21
cythia123 <cchenyue@gmail.com>
2026-08-16 14:45:24 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xiaobenzi-v1';
+const CACHE_NAME = 'xiaobenzi-v2';
 
 self.addEventListener('install', function (e) {
   self.skipWaiting();
@@ -8,17 +8,18 @@ self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keys) {
       return Promise.all(keys.filter(function (k) { return k !== CACHE_NAME; }).map(function (k) { return caches.delete(k); }));
+    }).then(function () {
+      return self.clients.claim();
     })
   );
-  self.clients.claim();
 });
 
-// 有网：优先用最新版本，同时存一份到缓存里
-// 没网：用上次存的缓存
+// 有网：强制去服务器要最新的（不许用手机自己的网页缓存），同时存一份到离线缓存里
+// 没网：用上次存的离线缓存
 self.addEventListener('fetch', function (e) {
   if (e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request)
+    fetch(e.request, { cache: 'no-store' })
       .then(function (res) {
         var copy = res.clone();
         caches.open(CACHE_NAME).then(function (cache) { cache.put(e.request, copy); });
